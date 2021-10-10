19a593d18bbf26cd9f52bc012509c72e0c4c0c53
Eldose Chacko M <eldhosechacko@gmail.com>
2021-10-10 15:04:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const tenantName    = 'ust-global.com';
+//const tenantName    = 'ust-global.com';
 //const clientID      = '40aee588-5f30-439e-a475-598e465e4c5b';
 const clientID      = "cd7ac348-4e26-4a45-9e8e-999619d45d0c";
 const serverPort    = 3000;
